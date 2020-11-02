27f3b3955e970228da236301afd1e5801a5094ae
472529 <472529@chrome.escg.ac.uk>
2020-11-02 20:13:19 +0000

    platformer pt2 - remade platformer game using raycasting making it much more relabile in game without the bugs of the other game - moving platforms that have a waypoint system - walljump abililty added - wallslide added - various bugs fixed     - character being able to launch themselves when jumping from underneath a platform     - player can climb and descend slopes reliably     - player can jump reliably with out it jumping before hitting the ground

---
+++ Platformer pt2/Assets/Scripts/Player.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+
+    float acceleractionTimeAirborne = .1f;
+    float acceleractionTimeGrounded = .2f;
+    float moveSpeed = 6;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .025f;
+    float timeToWallUnstick;
+
+    float gravity = -20;
+    float jumpVelocity = 8;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Controller2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D> ();
+
+        gravity = -(2 * jumpHeight)/Mathf.Pow (timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print ("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+    }
+
+    void Update() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // equal to -1 if a wall is to the left or 1 for right
+        int wallDirX = (controller.collisions.left)? -1 : 1;
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?acceleractionTimeGrounded:acceleractionTimeAirborne);
+
+        bool wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0){
+            wallSliding = true;
+
+            if (velocity.y < -wallSlideSpeedMax) {
+                velocity.y = -wallSlideSpeedMax;
+            }
+            if(timeToWallUnstick > 0){
+                velocityXSmoothing = 0;
+                velocity.x = 0;
+                if (input.x != wallDirX && input.x != 0){
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else{
+                    timeToWallUnstick = wallStickTime;
+                }
+            }
+            else{
+                timeToWallUnstick = wallStickTime;
+            }
+        }
+
+        if(controller.collisions.above || controller.collisions.below){
+            velocity.y = 0;
+        }
+
+        
+
+        if (Input.GetKeyDown (KeyCode.Space)){
+            if(wallSliding){
+                if (wallDirX == input.x){
+                    velocity.x = -wallDirX * wallJumpClimb.x;
+                    velocity.y = wallJumpClimb.y;
+                }
+                //when jumping off the wall
+                else if (input.x == 0){
+                    velocity.x = -wallDirX * wallJumpOff.x;
+                    velocity.y = wallJumpOff.y;
+                }
+                else{
+                    velocity.x = -wallDirX * wallLeap.x;
+                    velocity.y = wallLeap.y;
+                }
+            }
+            if(controller.collisions.below){
+            velocity.y = jumpVelocity;
+            }
+        }
+
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+}
