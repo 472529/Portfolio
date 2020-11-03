2a2d95a9b01791e99b567c128ae43cbeb1496eec
472529 <472529@chrome.escg.ac.uk>
2020-11-03 14:19:40 +0000

    completed and fIxed camera follow bugs such as the focus area of the player jittering around due to accidentally putting another - into the code. Another bug fixed was the camera not offsetting its direction this was due to simiarlly named varibles and me putting down them down in the wrong order. also added smooth varible for the x and y to the camera so if the player stops midway though offsetting it will stay with the player not just jump to the otherside jarringly. possible problem with big jumps though as the vertical smoothing would make the character appear of screen and then catch up as they hit the ground.

--- Platformer pt2/Assets/Scripts/CameraFollow.cs
+++ Platformer pt2/Assets/Scripts/CameraFollow.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Controller2D target;
+    public float verticalOffset;
+    public float lookAheadDstX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
     public Vector2 focusAreaSize;
+    
 
     FocusArea focusArea;
+
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
     void Start()
     {
         focusArea = new FocusArea(target.collider.bounds, focusAreaSize); 
@@ -16,6 +30,30 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         focusArea.Update(target.collider.bounds);
+
+        Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
+
+        if (focusArea.velocity.x != 0){
+            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            if(Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0){
+                lookAheadStopped = false;
+                targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            }
+            else{
+                if(!lookAheadStopped){
+                   lookAheadStopped = true;
+                   targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX)/4; 
+                }
+                
+            }
+        }
+
+        
+        currentLookAheadX = Mathf.SmoothDamp (currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp (transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
     }
 
     void OnDrawGizmos()
@@ -34,7 +72,7 @@ public class CameraFollow : MonoBehaviour
         public FocusArea(Bounds targetbounds, Vector2 size)
         {
             left = targetbounds.center.x - size.x / 2;
-            right = targetbounds.center.x - size.x / 2;
+            right = targetbounds.center.x + size.x / 2;
             bottom = targetbounds.min.y;
             top = targetbounds.min.y + size.y;
 
